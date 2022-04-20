cf01f23d84ba1579c797bf79ece02cdad2543653
Victoire Rey <97622659+Victoirey@users.noreply.github.com>
2022-04-20 17:21:48 +0200

    Update app.py

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -23,8 +23,7 @@ st.markdown(
 ###############################################################################
 ############### SIDEBAR #######################################################
 ###############################################################################
-st.sidebar.subheader("The bike revolution in Paris")
-st.sidebar.subheader("en route to #panamsterdam")
+st.sidebar.subheader("The bike revolution in Paris \n en route to #panamsterdam")
 page1 = "Executive summary"
 page2 = "Infrastructure overview"
 page3 = "Traffic overview"
@@ -47,15 +46,14 @@ st.sidebar.write("Olivier Masson " "\n" "[Linkedin](https://www.linkedin.com/in/
 ############### PAGE 1 - EXECUTIVE SUMMARY ####################################
 ###############################################################################
 if select_page == page1:
-    st.markdown("<h2>" "Executive summary" "</h2>", unsafe_allow_html=True)
-    st.markdown("coucou")
+    st.subheader("<h2>" "Executive summary" "</h2>", unsafe_allow_html=True)
+    st.markdown("PENDING")
 
 
 ###############################################################################
 ############### PAGE 2 - INFRASTRUCTURE OVERVIEW ##############################
 ###############################################################################
 elif select_page == page2:
-    st.markdown("Cycling path - map", unsafe_allow_html=True)
     def main():
         html_temp = """<div class='tableauPlaceholder' id='viz1650466156743' style='position: relative'><noscript><a href='#'><img alt='Tableau de bord 1 ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Cy&#47;CyclingpathsParis&#47;Tableaudebord1&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='CyclingpathsParis&#47;Tableaudebord1' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Cy&#47;CyclingpathsParis&#47;Tableaudebord1&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='fr-FR' /><param name='filter' value='publish=yes' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1650466156743');                    var vizElement = divElement.getElementsByTagName('object')[0];                    if ( divElement.offsetWidth > 800 ) { vizElement.style.width='1000px';vizElement.style.height='827px';} else if ( divElement.offsetWidth > 500 ) { vizElement.style.width='1000px';vizElement.style.height='827px';} else { vizElement.style.width='100%';vizElement.style.height='727px';}                     var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>"""
         components.html(html_temp, width=1000, height=500)
@@ -140,13 +138,11 @@ elif select_page == page7:
 
 
 
-    
-st.write("coucou")
-st.write("changement à voir")
 
-
-def main():
-    html_temp = """<div class='tableauPlaceholder' id='viz1650302350918' style='position: relative'><noscript><a href='#'><img alt=' ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Pa&#47;Panamsterdam_accidents2017_2020&#47;Bikeaccidentstrend&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='Panamsterdam_accidents2017_2020&#47;Bikeaccidentstrend' /><param name='tabs' value='yes' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Pa&#47;Panamsterdam_accidents2017_2020&#47;Bikeaccidentstrend&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='fr-FR' /><param name='filter' value='publish=yes' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1650302350918');                    var vizElement = divElement.getElementsByTagName('object')[0];                    vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';                    var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>"""
-    components.html(html_temp, width=1000, height=500)
-if __name__ == "__main__":    
-    main()
+# =============================================================================
+# def main():
+#     html_temp = """<div class='tableauPlaceholder' id='viz1650302350918' style='position: relative'><noscript><a href='#'><img alt=' ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Pa&#47;Panamsterdam_accidents2017_2020&#47;Bikeaccidentstrend&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='Panamsterdam_accidents2017_2020&#47;Bikeaccidentstrend' /><param name='tabs' value='yes' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Pa&#47;Panamsterdam_accidents2017_2020&#47;Bikeaccidentstrend&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='fr-FR' /><param name='filter' value='publish=yes' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1650302350918');                    var vizElement = divElement.getElementsByTagName('object')[0];                    vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';                    var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>"""
+#     components.html(html_temp, width=1000, height=500)
+# if __name__ == "__main__":    
+#     main()
+# =============================================================================
