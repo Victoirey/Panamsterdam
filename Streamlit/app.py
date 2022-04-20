53366231ef01bb8fdbedd8978b89dc37ef083e83
Victoire Rey <97622659+Victoirey@users.noreply.github.com>
2022-04-20 17:25:58 +0200

    Update app.py

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -13,11 +13,7 @@ st.set_page_config(page_title="#panamsterdam", page_icon=":bike:", layout="wide"
 
 #image = Image.open(r'C:\Users\Administrateur\Documents\GitHub\Final_Project\Streamlit\Panamsterdam.png')
 #st.image(image, use_column_width=True, width=500)
-st.markdown(
-"<h1 style='text-align: center'>"
-"<strong>The bike revolution in Paris (2018-2024):</strong>"
-"<br><span style='font-size: smaller'>en route to #panamsterdam</span>"
-"</h1>", unsafe_allow_html=True)
+
 
 
 ###############################################################################
@@ -46,6 +42,11 @@ st.sidebar.write("Olivier Masson " "\n" "[Linkedin](https://www.linkedin.com/in/
 ############### PAGE 1 - EXECUTIVE SUMMARY ####################################
 ###############################################################################
 if select_page == page1:
+    st.markdown(
+    "<h1 style='text-align: center'>"
+    "<strong>The bike revolution in Paris (2018-2024):</strong>"
+    "<br><span style='font-size: smaller'>en route to #panamsterdam</span>"
+    "</h1>", unsafe_allow_html=True)
     st.subheader("Executive summary")
     st.markdown("PENDING")
 
@@ -54,6 +55,7 @@ if select_page == page1:
 ############### PAGE 2 - INFRASTRUCTURE OVERVIEW ##############################
 ###############################################################################
 elif select_page == page2:
+    st.subheader("Infrastructure overview")
     def main():
         html_temp = """<div class='tableauPlaceholder' id='viz1650466156743' style='position: relative'><noscript><a href='#'><img alt='Tableau de bord 1 ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Cy&#47;CyclingpathsParis&#47;Tableaudebord1&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='CyclingpathsParis&#47;Tableaudebord1' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Cy&#47;CyclingpathsParis&#47;Tableaudebord1&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='fr-FR' /><param name='filter' value='publish=yes' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1650466156743');                    var vizElement = divElement.getElementsByTagName('object')[0];                    if ( divElement.offsetWidth > 800 ) { vizElement.style.width='1000px';vizElement.style.height='827px';} else if ( divElement.offsetWidth > 500 ) { vizElement.style.width='1000px';vizElement.style.height='827px';} else { vizElement.style.width='100%';vizElement.style.height='727px';}                     var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>"""
         components.html(html_temp, width=1000, height=500)
@@ -74,6 +76,7 @@ elif select_page == page2:
 ############### PAGE 3 - TRAFFIC OVERVIEW #####################################
 ###############################################################################
 elif select_page == page3:
+    st.subheader("Traffic overview")
     st.markdown(" ")
     
     
@@ -81,6 +84,7 @@ elif select_page == page3:
 ############### PAGE 4 - ACCIDENT OVERVIEW ####################################
 ###############################################################################
 elif select_page == page4:
+    st.subheader("Accident overview")
     st.markdown(" ")
     def main():
         html_temp = """<div class='tableauPlaceholder' id='viz1650300467898' style='position: relative'><noscript><a href='#'><img alt='Bike accidents map - 2020 ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Pa&#47;Panamsterdam_accidents2017_2020&#47;Bikeaccidentsmap&#47;1_rss.png' style='border: none' /></a></noscript><object allowfullscreen="true" class='tableauViz'  style='display:none;'>
@@ -114,6 +118,7 @@ elif select_page == page4:
 ############### PAGE 5 - CORRELATIONS #########################################
 ###############################################################################
 elif select_page == page5:
+    st.subheader("Correlation between variables")
     st.markdown(
     "<h1 style='text-align: center'>" "<strong>The bike revolution in Paris (2018-2024):</strong>" "<br><span style='font-size: smaller'>en route to #panamsterdam</span>" "</h1>", unsafe_allow_html=True)
 
@@ -121,6 +126,7 @@ elif select_page == page5:
 ############### PAGE 6 - USERS' PERCEPTION ####################################
 ###############################################################################
 elif select_page == page6:
+    st.subheader("Users' perception")
     st.markdown(
     "<h1 style='text-align: center'>" "<strong>The bike revolution in Paris (2018-2024):</strong>" "<br><span style='font-size: smaller'>en route to #panamsterdam</span>" "</h1>", unsafe_allow_html=True)
 
@@ -128,6 +134,7 @@ elif select_page == page6:
 ############### PAGE 7 - CONCLUSIONS OF PROJECT ###############################
 ###############################################################################
 elif select_page == page7:
+    st.subheader("Is Paris municipality giving itself the means to achieve its ambitions?")
     st.markdown(
     "<h1 style='text-align: center'>" "<strong>The bike revolution in Paris (2018-2024):</strong>" "<br><span style='font-size: smaller'>en route to #panamsterdam</span>" "</h1>", unsafe_allow_html=True)
 
