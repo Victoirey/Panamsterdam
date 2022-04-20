15e5082781458b9efaeaf35391d331abe5f01d7b
Victoire Rey <97622659+Victoirey@users.noreply.github.com>
2022-04-20 18:08:21 +0200

    Update app.py

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -74,8 +74,8 @@ elif select_page == page2:
         </object></div>                <script type='text/javascript'>                    
         var divElement = document.getElementById('viz1650469374490');                    
         var vizElement = divElement.getElementsByTagName('object')[0]; 
-        vizElement.style.width = '80%';        
-        vizElement.style.height = '1527px';                   
+        vizElement.style.width = '100%';        
+        vizElement.style.height = '100%';                   
         var scriptElement = document.createElement('script');                    
         scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    
         vizElement.parentNode.insertBefore(scriptElement, vizElement);                
